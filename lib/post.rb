242d97f0b7b85f8f43b6f12874155ae88e1b22e6
Thach Luu <luuphanbaothach@gmail.com>
2020-04-23 07:46:01 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,15 +1,3 @@
 class Post
-
   attr_accessor :title, :author
-  def initialize(title)
-    @title = title
-  end
-
-  def author_name
-    if @author == nil
-      return nil
-    else
-      @author.name
-    end
-  end
 end
